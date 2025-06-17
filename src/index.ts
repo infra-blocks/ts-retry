08d3c340df4d5908e2a9d2fc53b8ce6341f464d4
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-06-17 05:32:25 -0400

    First implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,174 @@
-export { testMe } from "./utils.js";
+import promiseRetry = require("promise-retry");
+import { EmitterLike, EmitterLikeBase, Predicate } from "@infra-blocks/types";
+
+/**
+ * Default retry configuration.
+ *
+ * The default configuration retries 60 times, with a factor of 1 and a minimum interval of 1000ms.
+ * The maximum interval is set to Infinity, meaning that there is no upper bound on the wait time.
+ *
+ * When no field is overridden, this config will result in retrying the function every second
+ * for a minute. Note that retries don't include the first attempt, so the inner function is
+ * called 61 times in total.
+ */
+export const DEFAULT_RETRY_CONFIG: Required<RetryConfig<unknown>> = {
+  retries: 60,
+  factor: 1,
+  minIntervalMs: 1000,
+  maxIntervalMs: Infinity,
+  isRetryableError: (): boolean => true,
+};
+
+/**
+ * Configuration for the retry function.
+ */
+export interface RetryConfig<E = Error> {
+  /**
+   * The amount of retries that will be attempted. Note that the first attempt
+   * doest not count as a retry. In total, the amount of retries plus one
+   * attempts will be made, in the worst case scenario.
+   */
+  retries?: number;
+  /**
+   * The exponential backoff factor that will be used between retries.
+   * The actual wait time can be calculated as such:
+   * wait time = min((factor ^ retry) * minIntervalMs, maxIntervalMs)
+   */
+  factor?: number;
+  /**
+   * The minimum wait time, in milliseconds, before the first retry.
+   * When the factor is 1, the wait time between retries is constant and
+   * equal to this value.
+   */
+  minIntervalMs?: number;
+  /**
+   * The maximum wait time between retries can be bound using this option.
+   */
+  maxIntervalMs?: number;
+  /**
+   * A predicate used to determine if an error should warrant a retry or not.
+   *
+   * When the function returns true, a retry will be attempted. When it returns
+   * false, the process immediately fails and throws the error.
+   *
+   * @param err - The error received from the inner function.
+   * @returns True if the function should be retried, false otherwise.
+   */
+  isRetryableError?: Predicate<E>;
+}
+
+/**
+ * Events and their handler types for the {@link Retry}.
+ */
+export type RetryEvents<E = Error> = {
+  /**
+   * This event is emitted at the beginning of every attempt.
+   *
+   * @param attempt - The attempt number. Starts with 1, and the first attempt does not count as a retry.
+   */
+  attempt: (params: {
+    attempt: number;
+    retryConfig: Required<RetryConfig<E>>;
+  }) => void;
+};
+
+/**
+ * Type returned by the {@link retry} function.
+ *
+ * This type is both a promise and an event emitter, where the events are described as in
+ * {@link RetryEvents}.
+ */
+export interface Retry<T, E = Error>
+  extends EmitterLike<RetryEvents<E>>,
+    PromiseLike<T> {}
+
+/**
+ * The type of the inner function that is wrapped by the {@link retry} function.
+ */
+export type RetryFunction<R> = () => Promise<R>;
+
+class RetryImpl<T, E>
+  extends EmitterLikeBase<RetryEvents<E>>
+  implements Retry<T, E>
+{
+  protected readonly promise: PromiseLike<T>;
+  protected readonly retryConfig: Required<RetryConfig<E>>;
+
+  constructor(fn: RetryFunction<T>, options?: RetryConfig<E>) {
+    super();
+    this.retryConfig = { ...DEFAULT_RETRY_CONFIG, ...options };
+
+    const wrapper = (attempt: number) => {
+      this.emit("attempt", {
+        attempt,
+        retryConfig: { ...this.retryConfig },
+      });
+      return fn();
+    };
+    this.promise = retryPromise(wrapper, this.retryConfig);
+  }
+
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?:
+      | ((value: T) => TResult1 | PromiseLike<TResult1>)
+      | undefined
+      | null,
+    onrejected?:
+      | ((reason: unknown) => TResult2 | PromiseLike<TResult2>)
+      | undefined
+      | null
+  ): PromiseLike<TResult1 | TResult2> {
+    return this.promise.then(onfulfilled, onrejected);
+  }
+}
+
+/**
+ * Returns a {@link Retry} object that wraps the provided function.
+ *
+ * @param fn - The inner function that will be retried.
+ * @param options - The configuration for the retry process, such as defined in {@link RetryConfig}.
+ *
+ * @returns A {@link Retry} that is configured with the provided arguments.
+ */
+export function retry<T, E = Error>(
+  fn: RetryFunction<T>,
+  options?: RetryConfig<E>
+): Retry<T, E> {
+  return new RetryImpl(fn, options);
+}
+
+/**
+ * The wraps a function into a retry promise.
+ *
+ * The promise resolves when the function resolves, or when the retry configuration determines so.
+ * The promise will reject is the inner function throws an error that is not retryable, or
+ * if the maximum amount of retries has been reached.
+ *
+ * @param fn - The inner function that will be retried.
+ * @param options - The retry configuration, such as defined in {@link RetryConfig}.
+ *
+ * @returns A promise wrapping the inner function with retry logic.
+ */
+async function retryPromise<T, E = Error>(
+  fn: (attempt: number) => Promise<T>,
+  options?: RetryConfig<E>
+): Promise<T> {
+  const { retries, factor, minIntervalMs, maxIntervalMs, isRetryableError } = {
+    ...DEFAULT_RETRY_CONFIG,
+    ...options,
+  };
+
+  return await promiseRetry(
+    { retries, factor, minTimeout: minIntervalMs, maxTimeout: maxIntervalMs },
+    async (retryInner, attempt) => {
+      try {
+        return await fn(attempt);
+      } catch (err) {
+        if (isRetryableError(err as E)) {
+          return retryInner(err);
+        }
+        throw err;
+      }
+    }
+  );
+}
--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,10 +1,126 @@
-import { testMe } from "../../src/index.js";
-import { expect } from "@infra-blocks/test";
+import { retry } from "../../src/index.js";
+import { expect, sinon } from "@infra-blocks/test";
+import { range } from "@infra-blocks/iter";
+import VError from "verror";
 
-describe("index", function () {
-  describe(testMe.name, function () {
-    it("should return the right stuff", function () {
-      expect(testMe()).to.equal("you tested me all right!");
+describe("retry", function () {
+  describe(retry.name, function () {
+    let clock: sinon.SinonFakeTimers;
+    beforeEach("setup timers", () => {
+      clock = sinon.useFakeTimers();
+    });
+
+    afterEach("tear down timers", () => {
+      clock.restore();
+    });
+
+    function waitTime(params: {
+      attempt: number;
+      factor: number;
+      minIntervalMs: number;
+    }): number {
+      const { attempt, factor, minIntervalMs } = params;
+      // We consider the attempt number the current one, but the wait time is calculated on the past number of
+      // retries. The first attempt does not count as a retry.
+      return Math.pow(factor, attempt - 2) * minIntervalMs;
+    }
+
+    async function test<
+      T
+    >(params: { inner: sinon.SinonSpy; retryPromise: PromiseLike<T>; retries: number; factor: number; minIntervalMs: number }): Promise<void> {
+      const { inner, retryPromise, retries, factor, minIntervalMs } = params;
+
+      const result = retryPromise.then(null, () => true);
+      // Run the callbacks submitted for next loop iteration.
+      await clock.tickAsync(1);
+      expect(inner).to.have.callCount(1);
+
+      for (const attempt of range(2, retries + 2)) {
+        await clock.tickAsync(waitTime({ attempt, factor, minIntervalMs }));
+        expect(inner).to.have.callCount(attempt);
+      }
+      // Do an extra one to show nothing changes.
+      await clock.tickAsync(
+        waitTime({ attempt: retries * 2, factor, minIntervalMs })
+      );
+      expect(inner).to.have.callCount(retries + 1);
+
+      await expect(result).to.eventually.be.true;
+    }
+
+    it("should default to retrying every second for 60 seconds", async function () {
+      const inner = sinon.fake.rejects(new Error("always failing"));
+      await test({
+        inner,
+        retryPromise: retry(inner),
+        retries: 60,
+        factor: 1,
+        minIntervalMs: 1000,
+      });
+    });
+
+    it("should respect the options", async function () {
+      const inner = sinon.fake.rejects(new Error("always failing"));
+      // You have to be careful with how large the numbers become, as it seems
+      // to not play well with fake timers.
+      const retries = 20;
+      const factor = 2;
+      const minIntervalMs = 500;
+      await test({
+        inner,
+        retryPromise: retry(inner, { retries, factor, minIntervalMs }),
+        retries,
+        factor,
+        minIntervalMs,
+      });
+    });
+
+    it("should respect the predicate to test errors with", async function () {
+      const inner = sinon.stub();
+      inner
+        .onFirstCall()
+        .rejects(new VError({ name: "RetryableError" }, "retry me pls"))
+        .onSecondCall()
+        .rejects(new Error("Should go boom"));
+      const isRetryableError = sinon.fake(
+        (err: Error) => err.name === "RetryableError"
+      );
+
+      let failedCorrectly = false;
+      retry(inner, { isRetryableError }).then(null, (err: Error) => {
+        // Using this approach to not trigger unhandled promise rejections, as would happen
+        // by just checking the promise at the end of the test.
+        expect(err.message).to.equal("Should go boom");
+        failedCorrectly = true;
+      });
+      await clock.tickAsync(1);
+      expect(inner).to.have.callCount(1);
+      expect(failedCorrectly).to.be.false;
+
+      await clock.tickAsync(
+        waitTime({ attempt: 2, factor: 1, minIntervalMs: 1000 })
+      );
+      expect(inner).to.have.callCount(2);
+
+      expect(failedCorrectly).to.be.true;
+    });
+
+    it("should stop after a success and resolve to the right value", async function () {
+      const inner = () => Promise.resolve(10);
+      const retryPromise = retry(inner).on("attempt", ({ attempt }) => {
+        if (attempt < 5) {
+          throw new Error("didn't try enough");
+        }
+      });
+      // Run the callbacks submitted for next loop iteration.
+      await clock.tickAsync(1);
+
+      for (const attempt of range(2, 6)) {
+        await clock.tickAsync(
+          waitTime({ attempt, factor: 1, minIntervalMs: 1000 })
+        );
+      }
+      await expect(retryPromise).to.eventually.equal(10);
     });
   });
 });
