8ce5dc39ba05827d46f6d497982ea481d6820e7a
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-12-30 09:34:35 -0500

    fixed emitter typing
    - Note that the previous bug didn't affect the public interface at all,
    just circumvented the proper type check on the `emit` method used
    internally. The now updated code correctly verifies the arguments
    passed to `emit`.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import {
-  type AlwaysVoid,
-  Emitter,
   type EmitterLike,
-  EmitterLikeBase,
+  IgnoringEachEmitterLikeBase,
 } from "@infra-blocks/emitter";
 import type { Predicate } from "@infra-blocks/types";
 import promiseRetry from "promise-retry";
@@ -112,14 +110,14 @@ export interface Retry<T, E = unknown>
 export type RetryFunction<R> = () => Promise<R>;
 
 class RetryImpl<T, E>
-  extends EmitterLikeBase<RetryEvents<E>, AlwaysVoid<RetryEvents<E>>>
+  extends IgnoringEachEmitterLikeBase<RetryEvents<E>>
   implements Retry<T, E>
 {
   protected readonly promise: PromiseLike<T>;
   protected readonly retryConfig: Required<RetryConfig<E>>;
 
   constructor(fn: RetryFunction<T>, options?: RetryConfig<E>) {
-    super({ emitter: Emitter.ignoringEach() });
+    super();
     this.retryConfig = { ...DEFAULT_RETRY_CONFIG, ...options };
 
     const wrapper = (attempt: number) => {
